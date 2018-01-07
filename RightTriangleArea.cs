fd54be2f7845a5480df9787a656dc0291ff4d0f2
AIvasiuc <alexandr.ivasiuc@gmail.com>
2018-01-07 18:50:49 +0200

    Initial commit

---
+++ RightTriangleArea.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RightTriangleAreaLib
+{
+    public class RightTriangleArea
+    {
+        //Let's calculate area using Heron's Formula
+        public double Area (double a, double b, double c)
+        {
+            //Calculating semiperimeter
+            double semiperimeter = (a + b + c) / 2;
+
+            //return result conform to Heron's formula
+            return Math.Sqrt(semiperimeter * (semiperimeter - a) * (semiperimeter - b) * (semiperimeter - c));
+
+        }
+    }
+}
---
+++ RightTriangleAreaTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RightTriangleAreaLib.Tests
+{
+    [TestClass]
+    public class RightTriangleAreaTests
+    {
+        [TestMethod]
+        public void Area_3and4and5_6returned()
+        {
+            // arrange
+            double a = 3;
+            double b = 4;
+            double c = 5;
+            double expected = 6;
+
+            // act
+            RightTriangleArea s = new RightTriangleArea();
+            double actual = s.Area(a, b, c);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
